76c7b5f8bb1cad501565b7144c5ab281fa3b023e
assadriaz <assad.riaz@entur.org>
2020-10-15 10:28:28 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,17 +9,3 @@ provider "google" {
 provider "kubernetes" {
   load_config_file = var.load_config_file
 }
-
-# create service account
-resource "google_service_account" "app_service_account" {
-  account_id   = "${var.labels.team}-${var.labels.app}-sa"
-  display_name = "${var.labels.team}-${var.labels.app} service account"
-  project = var.gcp_project
-}
-
-# add service account as member to the pubsub
-resource "google_project_iam_member" "project" {
-  project = var.pubsub_project
-  role    = var.sa_pubsub_role
-  member = "serviceAccount:${google_service_account.app_service_account.email}"
-}
