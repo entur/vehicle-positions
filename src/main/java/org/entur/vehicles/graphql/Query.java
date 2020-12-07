69ef71c24009169afa44efe6dfbc78142c5702ac
Lasse Tyrihjell <lassetyr@gmail.com>
2020-12-07 15:54:58 +0100

    Added request-logging

--- src/main/java/org/entur/vehicles/graphql/Query.java
+++ src/main/java/org/entur/vehicles/graphql/Query.java
@@ -5,9 +5,11 @@ import org.entur.vehicles.data.*;
 import org.entur.vehicles.repository.VehicleRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
+import java.util.UUID;
 
 @Component
 class Query implements GraphQLQueryResolver {
@@ -26,20 +28,31 @@ class Query implements GraphQLQueryResolver {
 
     Set<VehicleUpdate> getVehicles(String serviceJourneyId, String operator,
         String codespaceId, VehicleModeEnumeration mode, String vehicleId, String lineRef, String lineName, Boolean monitored, BoundingBox boundingBox) {
+        MDC.put("breadcrumbId", UUID.randomUUID().toString());
+
+        LOG.info("Finding vehicles");
         final Set<VehicleUpdate> vehicles = repository.getVehicles(new VehicleUpdateFilter(serviceJourneyId, operator, codespaceId, mode, vehicleId, lineRef, lineName, monitored, boundingBox));
         LOG.info("Returning {} vehicles", vehicles.size());
+
+        MDC.remove("breadcrumbId");
         return vehicles;
     }
 
     Set<Line> lines(String codespace) {
+        MDC.put("breadcrumbId", UUID.randomUUID().toString());
         final Set<Line> lines = repository.getLines(codespace);
         LOG.info("Returning {} lines", lines.size());
+        MDC.remove("breadcrumbId");
         return lines;
     }
 
     Set<Codespace> codespaces() {
+        MDC.put("breadcrumbId", UUID.randomUUID().toString());
+
         final Set<Codespace> codespaces = repository.getCodespaces();
         LOG.info("Returning {} codespaces", codespaces.size());
+
+        MDC.remove("breadcrumbId");
         return codespaces;
     }
 }
