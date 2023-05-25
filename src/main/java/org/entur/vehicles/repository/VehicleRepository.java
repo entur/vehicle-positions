8137eb7e63aa75cde985799478384b0f5770365f
Lasse Tyrihjell <lassetyr@gmail.com>
2023-05-25 15:49:17 +0200

    Setting empty operator-value

--- src/main/java/org/entur/vehicles/repository/VehicleRepository.java
+++ src/main/java/org/entur/vehicles/repository/VehicleRepository.java
@@ -149,17 +149,17 @@ public class VehicleRepository {
         v.setSpeed(Integer.valueOf(journey.getVelocity()).doubleValue());
       }
 
+      CharSequence operatorRef = journey.getOperatorRef();
+
       if (journey.getVehicleModes() != null && journey.getVehicleModes().size() > 0) {
         v.setMode(VehicleModeEnumeration.fromValue(journey.getVehicleModes().get(0)));
-      } else if (journey.getOperatorRef() != null) {
-          v.setMode(resolveModeByOperator(journey.getOperatorRef().toString()));
+      } else if (operatorRef != null) {
+          v.setMode(resolveModeByOperator(operatorRef.toString()));
       } else {
         v.setMode(VehicleModeEnumeration.BUS);
       }
 
-      if (journey.getOperatorRef() != null) {
-        v.setOperator(Operator.getOperator(journey.getOperatorRef().toString()));
-      }
+      v.setOperator(Operator.getOperator(operatorRef != null ? operatorRef.toString():""));
 
       if (journey.getDirectionRef() != null) {
         v.setDirection( journey.getDirectionRef().toString());
